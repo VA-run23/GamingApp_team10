fceefea6a45171fb612c1a365a1ea1136eabe6c4
VA-run23 <1523codes@gmail.com>
2025-09-16 16:17:10 +0530

    Initial commit

---
+++ GamingApp_team10/src/test/java/com/team10/GamingAppTeam10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingAppTeam10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
